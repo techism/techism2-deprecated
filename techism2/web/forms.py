6299e6de616a54fc27443ad669e10521caf30754
seelmann <mail@stefan-seelmann.de>
2010-11-08 11:54:39 +0100

    Sort location list

--- techism2/web/forms.py
+++ techism2/web/forms.py
@@ -11,7 +11,7 @@ class EventForm(forms.Form):
     date_time_end = forms.SplitDateTimeField(label=u'Ende (falls festgelegt)', required=False, input_date_formats=['%d.%m.%Y'], widget=forms.SplitDateTimeWidget(date_format='%d.%m.%Y', time_format='%H:%M'))
     url = forms.URLField()
     description = forms.CharField(label= u'Beschreibung', widget=forms.Textarea, required=False)
-    location = forms.ModelChoiceField (Location.objects.all(), required=False)
+    location = forms.ModelChoiceField (Location.objects.all().order_by('name'), required=False)
     tags = fields.CommaSeparatedListFormField(label= u'Tags', required=False)
     
     location_name = forms.CharField(label= u'Name',max_length=200, required=False)

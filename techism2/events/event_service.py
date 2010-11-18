c46f903249789af2243dc1f4c76ad357a821c7d8
seelmann <mail@stefan-seelmann.de>
2010-11-18 23:25:52 +0100

    Fixed event tag creation

--- techism2/events/event_service.py
+++ techism2/events/event_service.py
@@ -6,10 +6,10 @@ from techism2 import service
 tags_cache_key = "event_tags"
 
 def get_tags():
-    return service.get_tags(tags_cache_key, __get_base_event_query_set)
+    return service.get_tags(tags_cache_key, get_event_query_set)
 
 def update_tags_cache():
-    service.update_tags_cache(tags_cache_key, __get_base_event_query_set)
+    service.update_tags_cache(tags_cache_key, get_event_query_set)
 
 def __get_base_event_query_set():
     return Event.objects.filter(published__exact=True)

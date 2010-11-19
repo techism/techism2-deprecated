c3ff253a820ebd7647edd5d4065f2bbb5fbd1327
seelmann <mail@stefan-seelmann.de>
2010-11-19 09:03:13 +0100

    Remove check

--- techism2/rss/utils.py
+++ techism2/rss/utils.py
@@ -11,11 +11,9 @@ def get_change_log_prefix(event):
     else:
         change_log_items = EventChangeLog.objects.filter(event=event).filter(change_type=ChangeType.UPDATED).order_by('date_time')
         if change_log_items.exists():
-            change_type = change_log_items[0].change_type
-            if change_type == ChangeType.UPDATED:
-                if change_log_items.count() > 1:
-                    prefix = "[Update %s] " % str(change_log_items.count());
-                else:
-                    prefix = "[Update] ";
+            if change_log_items.count() > 1:
+                prefix = "[%s. Update] " % str(change_log_items.count());
+            else:
+                prefix = "[Update] ";
     
     return prefix

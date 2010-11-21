6049ac525be5aa36f136de591c383ebd43073cfe
cko <ch.ko123@googlemail.com>
2010-11-21 12:56:05 +0100

    fixed asynchronus problem

--- static/js/templates/event.js
+++ static/js/templates/event.js
@@ -1,13 +1,13 @@
 var geocoder;
 var map;
-var locations = new Object();
+var locations;
 google.maps.event.addDomListener(window, 'load', function() {
 	initializeMunichCityCenter();
-	update_location();
-  initialize_location();
 });
 
 $(function() {
+  initialize_location();
+  update_location();
 	$.datepicker.setDefaults( $.datepicker.regional[ "de" ] );
 	$("#id_date_time_begin_0").datepicker( $.datepicker.regional[ "de" ]);
 	$("#id_date_time_end_0").datepicker( $.datepicker.regional[ "de" ]);
@@ -15,14 +15,18 @@ $(function() {
 });
 
 var initialize_location = function() {
-    $.getJSON("/events/locations",{id: $(this).val(), ajax: 'true'}, function(jsonLocation){
-    /*var options = '<option value="">- - - - - - - - - -</option>';*/
-    for (var i = 0; i < jsonLocation.length; i++) {
-      /*options += '<option value="' + jsonLocation[i].id + '">' + jsonLocation[i].name + '</option>';*/
-      locations[jsonLocation[i].id] = jsonLocation[i];
-    }
-    $("#id_location").html(options);
-  });
+    if (locations == null){
+        $.getJSON("/events/locations",{id: $(this).val(), ajax: 'true'}, function(jsonLocation){
+        locations = new Object();
+        var options = '<option value="">- - - - - - - - - -</option>';
+        for (var i = 0; i < jsonLocation.length; i++) {
+          options += '<option value="' + jsonLocation[i].id + '">' + jsonLocation[i].name + '</option>';
+          locations[jsonLocation[i].id] = jsonLocation[i];
+        }
+        //$("#id_location").html(options);
+        update_location();
+        });
+  }
 }
 
 var update_location = function() {
@@ -37,7 +41,7 @@ var update_location = function() {
 		$("#id_location_city")
 			.val('')
 			.attr('disabled', false);
-	} else {
+	} else if (locations != null) {
 		$("#id_location_name")
 			.val(locations[id].name)
 			.attr('disabled', true);
